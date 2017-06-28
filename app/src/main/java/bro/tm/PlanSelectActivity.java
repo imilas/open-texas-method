c206868871d05abe96403fb1dde58c75c69a8a40
Amir Salimi <asalimi@ualberta.ca>
2017-06-27 23:47:23 -0600

    loading the correct plan in the plan spinner
    also modified powerlifting plan

--- app/src/main/java/bro/tm/PlanSelectActivity.java
+++ app/src/main/java/bro/tm/PlanSelectActivity.java
@@ -1,24 +1,13 @@
 package bro.tm;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TextView;
-
-import org.greenrobot.eventbus.EventBus;
-
-import events.DatabaseUpdate;
-import models.WeeklyMaxLogsHelper;
 
 /**
  * Created by Amir on 26/06/2017.
@@ -26,16 +15,12 @@ import models.WeeklyMaxLogsHelper;
 
 public class PlanSelectActivity extends FragmentActivity {
 
-
-//        planSpinner.setSelection(((ArrayAdapter)planSpinner.getAdapter()).getPosition(planFlavor));
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plan_select);
+        setContentView(R.layout.activity_plan_select);
         final SharedPreferences prefs = this.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-//        String planFlavor = prefs.getString("plan_type","Powerlifting TM");
 
         Spinner planSpinner = (Spinner) findViewById(R.id.plans_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -43,6 +28,8 @@ public class PlanSelectActivity extends FragmentActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         planSpinner.setAdapter(adapter);
 
+        String planFlavor = prefs.getString("plan_type","Powerlifting TM");
+        planSpinner.setSelection(((ArrayAdapter)planSpinner.getAdapter()).getPosition(planFlavor));
 
         planSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view,
